3c1216342abfe0d92e6b46a5a792947548ae2d17
Quang Huy <quanghuy19062002@gmail.com>
2024-09-19 15:16:50 +0700

    update role

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using cuahangbanle.Components.Pages.Manage;
+using cuahangbanle.Components.Services;
+using cuahangbanle.DBData.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace cuahangbanle.Data
@@ -17,6 +22,14 @@ namespace cuahangbanle.Data
             {
                 await roleManager.CreateAsync(new IdentityRole("Admin"));
             }
+            if (!await roleManager.RoleExistsAsync("Manager"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Manager"));
+            }
+            if (!await roleManager.RoleExistsAsync("Staff"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Staff"));
+            }
 
             // Seed admin user
             var adminEmail = "admin@gmail.com";
@@ -24,8 +37,14 @@ namespace cuahangbanle.Data
             if (adminUser == null)
             {
                 adminUser = new ApplicationUser { UserName = adminEmail, Email = adminEmail };
-                await userManager.CreateAsync(adminUser, "Admin@123"); // Set a strong password
+                await userManager.CreateAsync(adminUser, "Abc@123"); // Set a strong password
                 await userManager.AddToRoleAsync(adminUser, "Admin");
+                var userProfileService = serviceProvider.GetRequiredService<IUserProfileService>(); // Get the UserProfileService from the service provider
+                await userProfileService.CreateUserProfile(new UserProfile // Use the CreateUserProfileAsync method
+                {
+                    UserId = adminUser.Id,
+                    Hoten = "Admin"
+                });
             }
         }
     }
