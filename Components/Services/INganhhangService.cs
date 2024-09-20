69251389a42c8a6c692a16f730c4cbf4abb7f215
Quang Huy <quanghuy19062002@gmail.com>
2024-09-20 15:44:02 +0700

    update chức năng ngành hàng

--- Components/Services/INganhhangService.cs
+++ Components/Services/INganhhangService.cs
@@ -4,6 +4,10 @@ namespace cuahangbanle.Components.Services
 {
     public interface INganhhangService
     {
+        Task CreateNganhhang(Nganhhang nganh);
         Task<List<Nganhhang>> GetAllNganhhang();
+        Task<Nganhhang> GetNganhhangById(string id);
+        Task RemoveNganhhang(Nganhhang nganh);
+        Task UpdateNganhhang(Nganhhang nganh);
     }
 }
--- Components/Services/NganhhangService.cs
+++ Components/Services/NganhhangService.cs
@@ -1,6 +1,7 @@
 ﻿using cuahangbanle.Data;
 using cuahangbanle.DBData.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace cuahangbanle.Components.Services
 {
@@ -16,6 +17,29 @@ namespace cuahangbanle.Components.Services
         public async Task<List<Nganhhang>> GetAllNganhhang()
         {
             return await _db.Nganhhangs.ToListAsync();
+        }
+        public async Task<Nganhhang> GetNganhhangById(string id)
+        {
+            return await _db.Nganhhangs.FirstOrDefaultAsync(n => n.Manganhhang == id) ?? new();
+        }
+        #endregion
+
+        #region CUD
+        public async Task RemoveNganhhang(Nganhhang nganh)
+        {
+            _db.Nganhhangs.Remove(nganh);
+            await _db.SaveChangesAsync();
+        }
+        public async Task UpdateNganhhang(Nganhhang nganh)
+        {
+            _db.Nganhhangs.Update(nganh);
+            await _db.SaveChangesAsync();
+        }
+        public async Task CreateNganhhang(Nganhhang nganh)
+        {
+
+            await _db.Nganhhangs.AddAsync(nganh);
+            await _db.SaveChangesAsync();
         }
         #endregion
     }
